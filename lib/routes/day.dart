0f57711974b4a2beb53ed4bd6117c678d41eeb23
FirstSS-Sub <sota.monokuma-bass@docomo.ne.jp>
2020-05-11 04:48:57 +0900

    基本の画面構成作成

---
+++ lib/routes/day.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Day extends StatelessWidget { // <- (※1)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("20XX年 YY月 ZZ日"), // <- (※2)
+      ),
+      body: Center(child: Text("日にちのページ") // <- (※3)
+      ),
+    );
+  }
+}
