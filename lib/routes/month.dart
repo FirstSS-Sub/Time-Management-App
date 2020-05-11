16a499800745af942b4c0cb619d0e6b9a6912fca
FirstSS-Sub <sota.monokuma-bass@docomo.ne.jp>
2020-05-11 15:45:48 +0900

    基本の画面構成作成

---
+++ lib/routes/month.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Month extends StatelessWidget { // <- (※1)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("20XX年 YY月"), // <- (※2)
+      ),
+      body: Center(child: Text("月のページだよ") // <- (※3)
+      ),
+    );
+  }
+}
